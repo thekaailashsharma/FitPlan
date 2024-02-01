b5ff61f1e600caaae779be2ce6def7107fbf5e4e
Kailash Sharma <kailashps.1011@gmail.com>
2024-02-01 19:28:17 +0530

    Minor Bug Fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,7 +118,7 @@ dependencies {
     implementation(project(":feature:onboarding:presentation"))
 
 
-
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
